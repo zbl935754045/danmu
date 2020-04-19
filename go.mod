d2eaeda6bec4557f3df855765ee1a691a2309300
eloizhang <eloizhang@tencent.com>
2020-04-19 15:15:34 +0800

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module eloizhang/danmu
+
+go 1.13
+
+require (
+	github.com/Shopify/sarama v1.26.1
+	github.com/gin-gonic/gin v1.6.2
+	github.com/jinzhu/gorm v1.9.12
+)
---
+++ routers/router.go
@@ -0,0 +1,16 @@
+package routers
+
+import (
+	"eloizhang/danmu/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouter() *gin.Engine {
+
+	router := gin.Default()
+	router.LoadHTMLGlob("views/*")
+	//注册：
+	router.GET("/register",controllers.RegisterGet)
+	return router
+
+}
